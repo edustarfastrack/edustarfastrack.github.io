1dbddeba6c794eed2b7c36336090c4fd62f7c54b
edustarfastrack <edustarfastrack@gmail.com>
2021-06-10 18:51:07 +0530

    update

--- src/components/WhatsappButton.svelte
+++ src/components/WhatsappButton.svelte
@@ -1,5 +1,5 @@
 <div class="">
-    <a href="https://api.whatsapp.com/send?phone=919745971753&text=">
+    <a href="https://api.whatsapp.com/send?phone=919744412045&text=">
         <div class="fixed right-5 bottom-5 bg-blue-500 text-white text-4xl rounded-full w-16 h-16 flex justify-center items-center hover:bg-blue-400">
             <i class="fab fa-whatsapp text-center"></i>
         </div>
